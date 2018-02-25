f6891d150916c45ad74e990d1a4b0fc8fd84f594
khalidazia11 <khalida34@gmail.com>
2018-02-25 15:18:56 +0700

    push yang ketiga kali (modul3)

---
+++ app/src/main/java/com/example/khalidaziaamrina/khalidaziaamrina_1202150282_modul3/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.khalidaziaamrina.khalidaziaamrina_1202150282_modul3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), SignIn.class));
+
+                finish();
+            }
+
+        }, 5000); //3000 L = 3 detik
+    }
+}
---
+++ app/src/main/java/com/example/khalidaziaamrina/khalidaziaamrina_1202150282_modul3/coba.java
@@ -0,0 +1,8 @@
+package com.example.khalidaziaamrina.khalidaziaamrina_1202150282_modul3;
+
+/**
+ * Created by khalida zia amrina on 2/25/2018.
+ */
+
+public class coba {
+}
